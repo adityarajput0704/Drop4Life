ecd940ff84de87609ee4698165b027ae681577ca
Aditya Rajput <rajput.aditya.tech@gmail.com>
2026-04-09 09:40:40 +0530

    IP updated

--- App/lib/config/app_config.dart
+++ App/lib/config/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   static const bool useMockData = false; // ← FLIP THIS
 
-  static const String apiBaseUrl = 'http://192.168.0.102:8000';
+  static const String apiBaseUrl = 'http://10.40.73.75:8000';
 }
